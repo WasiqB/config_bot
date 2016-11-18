aacb3cdb0ce511aa22bcd36c508099cbbf13d8b1
Wasiq Bhamla <wasiq.bhamla@Wasiqs-Mac-mini.local>
2016-11-18 13:44:59 +0530

    Resolved issue in new command wrt default value for select option.

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+Nov 17, 2016
+v0.1.0
+- Initial release.
--- lib/config_bot/cli/new.rb
+++ lib/config_bot/cli/new.rb
@@ -56,60 +56,69 @@ module ConfigBot
       question.id = prompt.ask "#{question_id}.1. What is the unique id for the question?", \
         required: true
       type = prompt.select "#{question_id}.2. What is the type of question u want to ask?", \
-        Common::TYPES, required: true
+        Common::TYPES, required: true, default: 1
       question.type = Common::TYPES[type - 1]
       question.query = prompt.ask "#{question_id}.3. What question you want to ask?", \
         required: true
       convert = prompt.select "#{question_id}.4. What type you want the question input to be converted to?", \
         Common::CONVERTS, required: true, default: 10
       question.convert = Common::CONVERTS[convert - 1].to_sym
-      question.default = prompt.ask "#{question_id}.5. What default value you expect for your question?", \
-        convert: question.convert
-      question.required = prompt.yes? "#{question_id}.6. Is your question mandatory?", \
+      question.required = prompt.yes? "#{question_id}.5. Is your question mandatory?", \
         default: false, convert: :bool
-      if prompt.yes? "#{question_id}.7. Do your question expects any results?", \
-          required: true, convert: :bool
-        result_count = prompt.ask "#{question_id}.7.1. How many results are there?", \
-          required: true, convert: :int
-        (1..result_count).each do |i|
-          value = prompt.ask "#{question_id}.7.1.#{i}.1. What value this result can have?", \
-            required: true, convert: question.convert
-          result = Result.new value
-          question_count = prompt.ask "#{question_id}.7.1.#{i}.2. How many questions your result have?", \
-            required: true, convert: :int
-          (1..question_count).each do |id|
-            result.questions.push set_question id, "#{question_id}.7.1.#{id}.2."
-          end
-          question.add_result result
-        end
-      end
       case question.type
       when "mask"
-        question.mask = prompt.ask "#{question_id}.8. What mask value you want for your question?", \
+        question.default = prompt.ask "#{question_id}.6. What default value you expect for your question?", \
+          convert: question.convert
+        question.mask = prompt.ask "#{question_id}.7. What mask value you want for your question?", \
           required: true
       when "yes?"
-        question.positive = prompt.ask "#{question_id}.8.1. What is the positive value you want?", \
+        question.default = prompt.ask "#{question_id}.6. What default input you expect for your question?", \
+          convert: question.convert
+        question.positive = prompt.ask "#{question_id}.7.1. What is the positive value you want?", \
           required: true
-        question.negative = prompt.ask "#{question_id}.8.2. What is the negative value you want?", \
+        question.negative = prompt.ask "#{question_id}.7.2. What is the negative value you want?", \
           required: true
       when "select", "multi_select", "enum_select"
-        choice_count = prompt.ask "#{question_id}.8.1. How many choices are there for your question?", \
+        choice_count = prompt.ask "#{question_id}.6.1. How many choices are there for your question?", \
           required: true, convert: :int
         (1..choice_count).each do |i|
-          item = prompt.ask "#{question_id}.8.1.#{i}.1. Add choice:", required: true
-          id = prompt.ask "#{question_id}.8.1.#{i}.2. Choice id:", \
+          item = prompt.ask "#{question_id}.6.1.#{i}.1. Add choice:", required: true
+          id = prompt.ask "#{question_id}.6.1.#{i}.2. Choice id:", \
             required: true, convert: question.convert
           choice = Choice.new item, id
           question.add_choice choice
         end
-        question.help_msg = prompt.ask "#{question_id}.8.2. What help message u want?"
+        question.help_msg = prompt.ask "#{question_id}.6.2. What help message u want?"
+        question.default = prompt.ask "#{question_id}.7. What default choice id you expect for your question?", \
+          convert: :int
       when "slider"
-        question.min = prompt.ask "#{question_id}.8.1. What is the min value for slider?", \
+        question.default = prompt.ask "#{question_id}.6. What default value you expect for your question?", \
+          convert: question.convert
+        question.min = prompt.ask "#{question_id}.7.1. What is the min value for slider?", \
           required: true, convert: :int
-        question.max = prompt.ask "#{question_id}.8.2. What is the max value for slider?", \
+        question.max = prompt.ask "#{question_id}.7.2. What is the max value for slider?", \
           required: true, convert: :int
-        question.step = prompt.ask "#{question_id}.8.3. What is the step value for slider?", \
+        question.step = prompt.ask "#{question_id}.7.3. What is the step value for slider?", \
           required: true, convert: :int
+      end
+      case question.type
+      when "yes?", "select", "multi_select", "enum_select"
+        if prompt.yes? "#{question_id}.8. Do your question expects any results?", \
+            required: true, convert: :bool
+          result_count = prompt.ask "#{question_id}.8.1. How many results are there?", \
+            required: true, convert: :int
+          (1..result_count).each do |i|
+            value = prompt.ask "#{question_id}.8.1.#{i}.1. What value this result can have?", \
+              required: true, convert: question.convert
+            result = Result.new value
+            question_count = prompt.ask "#{question_id}.8.1.#{i}.2. How many questions your result have?", \
+              required: true, convert: :int
+            (1..question_count).each do |id|
+              result.questions.push set_question id, "#{question_id}.8.1.#{id}.2."
+            end
+            question.add_result result
+          end
+        end
       end
       question
     end
